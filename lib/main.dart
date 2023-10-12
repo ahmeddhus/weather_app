4b5c3b97f80d4feef0d58fabb9997be05897a2f5
Ahmed <ahmed.hussein@zarcony.dev>
2023-10-12 10:33:01 +0300

    complete MaterialApp widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/src/features/weather/presentation/weather_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,27 +10,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textStyleWithShadow = TextStyle(color: Colors.white, shadows: [
+      BoxShadow(
+        color: Colors.black12.withOpacity(0.25),
+        spreadRadius: 1,
+        blurRadius: 4,
+        offset: const Offset(0, 0.5),
+      )
+    ]);
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Weather App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        brightness: Brightness.light,
+        textTheme: TextTheme(
+          displayLarge: textStyleWithShadow,
+          displayMedium: textStyleWithShadow,
+          displaySmall: textStyleWithShadow,
+          headlineMedium: textStyleWithShadow,
+          headlineSmall: textStyleWithShadow,
+          titleMedium: const TextStyle(color: Colors.white),
+          bodyMedium: const TextStyle(color: Colors.white),
+          bodyLarge: const TextStyle(color: Colors.white),
+          bodySmall: const TextStyle(color: Colors.white70, fontSize: 13),
+        ),
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Home Screen'),
-      ),
+      home: const WeatherPage(),
     );
   }
 }
