1cfda82fd162a726cfc5cee6f7142627caee95dd
Ahmed <ahmed.hussein@zarcony.dev>
2023-10-12 19:46:04 +0300

    Add provider scope

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weather_app/src/features/weather/presentation/weather_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
