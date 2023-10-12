918603de6e6eab6669635b764fdc64d504fc2ffd
Ahmed <ahmed.hussein@zarcony.dev>
2023-10-12 10:36:29 +0300

    Add home screen background

--- lib/src/features/weather/presentation/weather_page.dart
+++ lib/src/features/weather/presentation/weather_page.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/src/constants/app_colors.dart';
 
 class WeatherPage extends StatelessWidget {
   const WeatherPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: AppColors.rainGradient,
+          ),
+        ),
+      ),
+    );
   }
 }
